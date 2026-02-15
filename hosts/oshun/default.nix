661ec16f79d2112e6abaf974e7cba3dad955a25a
Daniel Verrall <djverrall@gmail.com>
2026-02-15 13:47:46 +0000

    Fix renamed noto-fonts-emoji package

--- hosts/oshun/default.nix
+++ hosts/oshun/default.nix
@@ -135,7 +135,7 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       jetbrains-mono
       font-awesome
       nerd-fonts.jetbrains-mono
