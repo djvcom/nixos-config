19cc1030a6ee19620fb6334b7eaeb03be5d66ea8
Daniel Verrall <djverrall@gmail.com>
2026-03-14 19:27:10 +0000

    add gstreamer plugins for game cinematics and update flake inputs

--- modules/features/desktop/gaming.nix
+++ modules/features/desktop/gaming.nix
@@ -42,6 +42,12 @@ _:
         wine
         winetricks
         xenia-canary
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
       ];
     };
 }
