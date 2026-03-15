7d15ddf66f207f148eb27852157d00e6a617a17e
Daniel Verrall <djverrall@gmail.com>
2026-03-15 20:46:12 +0000

    expand nix-ld libraries for native game and UE5 compatibility

--- modules/features/base/packages.nix
+++ modules/features/base/packages.nix
@@ -31,6 +31,28 @@
         enable = true;
         libraries = with pkgs; [
           stdenv.cc.cc.lib
+          glib
+          nss
+          nspr
+          dbus
+          atk
+          at-spi2-atk
+          libdrm
+          xorg.libX11
+          xorg.libXcomposite
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXfixes
+          xorg.libXrandr
+          xorg.libxcb
+          mesa
+          libgbm
+          expat
+          libxkbcommon
+          pango
+          cairo
+          alsa-lib
+          at-spi2-core
         ];
       };
     };
